b1e8cb202af52d0d1775fd0c3b38d992d83da13e
miguelpracticagit <spavon17@gmail.com>
2023-03-21 10:34:11 +0100

    commit desde miguel_secundaria1

--- CuentaBancaria.java
+++ CuentaBancaria.java
@@ -1,12 +1,9 @@
 class TestCuentaBancaria
 {
-	public static void main(String args[])
-	{
-<<<<<<< HEAD
-		CuentaBancarai cuenta = new CuentaBancaria("123456789", "MIguel PAvon",1000,5)
-=======
+	public static void main(String ar)
+		
+
 		Cuentabancaria cuenta = new CuentaBancaria("0123456789", "Miguel Pavon",1000,5)
->>>>>>> migguel_secundaria1
 		CuentaBancaria cuenta = new CuentaBancaria("1234521","Director Banco",100,0);
 		System.out.println("Saldo = " + cuenta.getSaldo());
 		cuenta.reintegro(50);
